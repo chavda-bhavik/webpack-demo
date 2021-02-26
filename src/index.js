63bedcf3ea2770c1b925153e464e39144a238517
chavda-bhavik <bhavik.chvda@gmail.com>
2021-02-26 15:29:32 +0530

    wrapped out asset management for output management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-const { divide } = require("lodash");
 const _ = require("lodash");
-import './style.css'
-import DemoImage from './image.jpg'
-import CSV from './data.csv'
-import XML from './Books.xml'
 
 function component() {
     const element = document.createElement('div');
@@ -11,16 +6,6 @@ function component() {
     // importing and using lodash
     element.innerHTML = _.join(['Hello','Webpack'], ' ');
     element.classList.add('hello');
-
-    // adding image to existing div
-    const myImage = new Image();
-    myImage.src = DemoImage;
-    myImage.classList.add('image');
-    element.appendChild(myImage);
-
-    // adding data
-    console.log(CSV);
-    console.log(XML);
     
     return element;
 }
