d0d894ca3dd672fe0d7bad321c6cd974181ebb62
chavda-bhavik <bhavik.chvda@gmail.com>
2021-02-26 15:43:55 +0530

    output management through html and clean webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const _ = require("lodash");
+import printMe from './print'
 
 function component() {
     const element = document.createElement('div');
-
+    const button = document.createElement('button');
+    button.innerText = "ClickMe to PrintMe";
+    button.onclick = printMe
+    
     // importing and using lodash
     element.innerHTML = _.join(['Hello','Webpack'], ' ');
     element.classList.add('hello');
     
+    element.appendChild(button);
     return element;
 }
 
